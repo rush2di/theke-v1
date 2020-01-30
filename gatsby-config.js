c8d07976ed2e0dfcc265df391705e0f1b2300865
Rochdi <roch2di@gmail.com>
2020-01-30 23:27:10 +0100

    fix: graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title:'Theke',
+    developedBy: 'Rochdi Belhirch'
+  },
   plugins:[
     'gatsby-plugin-sass',
     'gatsby-plugin-netlify-cms'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Home = () => {
-    return (
-        <Layout>
-            <div>hello world</div>
-        </Layout>
-    )
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          developedBy
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <div>hello world</div>
+      <div>
+        {data.site.siteMetadata.title} developed by{" "}
+        {data.site.siteMetadata.developedBy}
+      </div>
+    </Layout>
+  )
 }
 
 export default Home
