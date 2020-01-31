7cbbc3efec8245ed83d46c0ab78b462e8e95fda0
Rochdi <roch2di@gmail.com>
2020-01-31 01:31:55 +0100

    fix: gatsby plugin remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins:[
     'gatsby-plugin-sass',
     'gatsby-plugin-netlify-cms',
+    'gatsby-transformer-remark'
     {
       resolve: 'gatsby-source-filesystem',
       options: {
