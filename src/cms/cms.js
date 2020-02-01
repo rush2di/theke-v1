7b1120416ddfea797bc4675cb90abc9cb6069e80
Rochdi <roch2di@gmail.com>
2020-02-01 03:59:33 +0100

    fix: cms config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,23 +1,23 @@
 import CMS from "netlify-cms-app"
 
-const TagsPreview = createClass({
-  render: function() {
-    const { value, fieldsMetaData } = this.props
-    const post = fieldsMetaData && fieldsMetaData.getIn(["tags", value])
-    const style = {
-      border: "2px solid #ccc",
-      borderRadius: "8px",
-      padding: "20px",
-    }
-    return post
-      ? h(
-          "div",
-          { style: style },
-          h("h2", {}, "tags"),
-          h("h3", {}, post.get("tag"))
-        )
-      : null
-  },
-})
+// export const TagsPreview = createClass({
+//   render: function() {
+//     const { value, fieldsMetaData } = this.props
+//     const post = fieldsMetaData && fieldsMetaData.getIn(["tags", value])
+//     const style = {
+//       border: "2px solid #ccc",
+//       borderRadius: "8px",
+//       padding: "20px",
+//     }
+//     return post
+//       ? h(
+//           "div",
+//           { style: style },
+//           h("h2", {}, "tags"),
+//           h("h3", {}, post.get("tag"))
+//         )
+//       : null
+//   },
+// })
 
-CMS.registerWidget("tagswidget",'related', TagsPreview)
+// CMS.registerWidget("tagswidget",'related', TagsPreview)
