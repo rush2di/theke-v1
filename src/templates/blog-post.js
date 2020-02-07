1420d4387893372a49db41545ee9ca8be7f85978
Rochdi <roch2di@gmail.com>
2020-02-07 14:54:28 +0100

    Update: blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
 const BlogPost = props => {
   props && console.log(props)
-  return <div>hello</div>
+  return (
+    <Layout>
+      <div className="article-wrapper">
+        Hello
+      </div>
+    </Layout>
+  )
 }
+
 
 export default BlogPost
