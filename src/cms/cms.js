ba6166932dd6e04e554003d3efa44ac9f6d831f0
Rochdi <roch2di@gmail.com>
2020-02-15 00:32:48 +0100

    Update: CMS previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,26 +1,7 @@
-import CMS from 'netlify-cms-app';
-import { fr } from 'netlify-cms-locales';
+import CMS from "netlify-cms-app"
+import { fr } from "netlify-cms-locales"
+import "../components/styles/index.scss"
+import PostPreview from "./previews/postPreview"
 
-CMS.registerLocale('fr', fr);
-
-// export const TagsPreview = createClass({
-//   render: function() {
-//     const { value, fieldsMetaData } = this.props
-//     const post = fieldsMetaData && fieldsMetaData.getIn(["tags", value])
-//     const style = {
-//       border: "2px solid #ccc",
-//       borderRadius: "8px",
-//       padding: "20px",
-//     }
-//     return post
-//       ? h(
-//           "div",
-//           { style: style },
-//           h("h2", {}, "tags"),
-//           h("h3", {}, post.get("tag"))
-//         )
-//       : null
-//   },
-// })
-
-// CMS.registerWidget("tagswidget",'related', TagsPreview)
+CMS.registerLocale("fr", fr)
+CMS.registerPreviewTemplate("articles", PostPreview)
---
+++ src/cms/previews/postPreview.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react"
+
+class PostPreview extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			src: "",
+		}
+	}
+	subscribed = true
+
+	fetchAsset = () => {
+		const path = this.props.entry.getIn(["data", "coverture"])
+		path &&
+			this.props.getAsset(path).then(res => {
+				if (this.subscribed) {
+					this.setState({ src: res.toString() })
+				}
+			})
+	}
+
+	componentDidMount() {
+		this.fetchAsset()
+	}
+
+	componentWillUnmount() {
+		this.subscribed = false
+	}
+
+	componentDidUpdate(prevProps) {
+		const prevPath = prevProps.entry.getIn(["data", "coverture"])
+		const path = this.props.entry.getIn(["data", "coverture"])
+		if (prevPath !== path || prevProps.getAsset !== this.props.getAsset) {
+			this.fetchAsset()
+		}
+	}
+
+	render() {
+		console.log(this.props.widgetFor('body'))
+		return (
+			<React.Fragment>
+				<div className="article_wrapper">
+					<div className="article_head">
+						<div
+							className="article_head--bg"
+							style={{ backgroundImage: `url(${this.state.src})` }}
+						/>
+						<h3>{this.props.entry.getIn(["data", "titre"])}</h3>
+					</div>
+					<div className="article_body">
+						{this.props.widgetFor('body')}
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	}
+}
+
+export default PostPreview
